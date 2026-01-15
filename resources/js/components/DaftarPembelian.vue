a95ad5c19b3a5c4cf160efe5ff01a5e57ef7a34a
Tutsnaa <sujanaiketut01@gmail.com>
2026-01-15 16:13:44 +0800

    Perbaikan-15-1-2026/1

--- resources/js/components/DaftarPembelian.vue
+++ resources/js/components/DaftarPembelian.vue
@@ -272,16 +272,7 @@ export default {
             this.editIndex = null;
             this.bayar = 0; // Reset input bayar
             console.log(this.daftarPembelian);
-             this.errorNama = '';
-
-        // VALIDASI
-        if (!this.namapelanggan || this.namapelanggan.trim() === '') {
-            this.errorNama = 'Nama pelanggan wajib diisi';
-            return; // hentikan proses simpan
-        }
-
-        // ✔️ LANJUT SIMPAN DATA
-        console.log('Data disimpan:', this.namapelanggan);
+            
         },
 
         // Emit perubahan item
